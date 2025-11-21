275d2ed607492f3f2ac74b9e2e4923025b12c87e
Yunus Emre Koyun <yunusemrekoyun26@gmail.com>
2025-11-21 15:03:56 +0300

    placements on stage
    Supports with the weights fall added with the style.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const startButton = document.getElementById("start-button");
-
+const gamescene = document.getElementById("game-scene");
 startButton.addEventListener("click", () => {
   console.log("game started");
+
+  gamescene.style.display = "block";
+  gamescene.style.pointerEvents = "auto";
 });
